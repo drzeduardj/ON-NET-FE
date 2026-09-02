b4e0ed01c7cb2f4824b8e9d2924059a1d551efea
EDUARDJ1001 <123607036+EDUARDJ1001@users.noreply.github.com>
2026-09-01 23:32:33 -0600

    Commit para deploy

--- src/app/components/cotizacion.tsx
+++ src/app/components/cotizacion.tsx
@@ -332,6 +332,7 @@ const Cotizacion = () => {
             </table>
           </div>
 
+          
           <div class="footer" style="padding-bottom: 20px;">
             <img 
               src="${window.location.origin}/img/ON-NET-BANNER.jpeg"
